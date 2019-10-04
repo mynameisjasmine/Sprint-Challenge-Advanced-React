04672b5155163b78fd7ea918daf540a6a5358429
Jasmine <jasminecharles@hotmail.com>
2019-10-04 10:53:44 -0700

    read me questions

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import App from './App';
+import Nav from './Nav';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders without crashing', () => {
+
+     render(<App />, div);
+
 });
+
+test('Is dark mode false?', () => {
+  render(<Nav />, div);
+})
